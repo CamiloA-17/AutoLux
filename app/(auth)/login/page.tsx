c951a3c6e27eceb644742d48dd2d8620d5188aa4
Camilo Andres Molano Aristizabal <camiangi1517@gmail.com>
2024-09-05 21:28:07 -0500

    Form for login

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next"
+import './style.css'
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "This is my login.",
+}
+
+export default function LoginPage() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="user">user</label>
+        <input id="user" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <div>
+        <input type="submit" value="Login" />
+      </div>
+    </form>
+  )
+}
