44ecf1e523530fa8935f7702c0a74f224a5b4642
juanesbrice11 <juanesbrice977@gmail.com>
2024-09-05 23:31:56 -0500

    Creando formulario login

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -15,12 +15,11 @@ export default function register() {
               Register
             </h2>
           </div>
-
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" className="space-y-6">
               <div>
                 <label htmlFor="full-name" className="block text-sm font-extrabold font-medium leading-6 text-[#EEEEEE]">
-                  Full Name
+                  Nombre Completo
                 </label>
                 <div className="mt-2">
                   <input
@@ -29,14 +28,14 @@ export default function register() {
                     type="text"
                     required
                     autoComplete="name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div>
                 <label htmlFor="email" className="block text-sm font-extrabold font-medium leading-6 text-[#EEEEEE]">
-                  Email address
+                  Correo electrónico
                 </label>
                 <div className="mt-2">
                   <input
@@ -45,14 +44,14 @@ export default function register() {
                     type="email"
                     required
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div>
                 <label htmlFor="password" className="block text-sm font-extrabold font-medium leading-6 text-[#EEEEEE]">
-                  Password
+                  Contraseña
                 </label>
                 <div className="mt-2">
                   <input
@@ -61,14 +60,14 @@ export default function register() {
                     type="password"
                     required
                     autoComplete="new-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div>
                 <label htmlFor="confirm-password" className="block text-sm font-extrabold font-medium leading-6 text-[#EEEEEE]">
-                  Confirm Password
+                  Confirmar contraseña
                 </label>
                 <div className="mt-2">
                   <input
@@ -77,7 +76,7 @@ export default function register() {
                     type="password"
                     required
                     autoComplete="new-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
