00a0cec83c31740ed8c653b84b27e87ac2aef2bc
slesmes <santiagolesmesmarin@hotmail.com>
2024-09-15 19:02:56 -0500

    realizando el about del home

--- app/(principal)/home/page.tsx
+++ app/(principal)/home/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import {About} from '@/components'
 
 export const metadata: Metadata = {
   title: "Home",
@@ -7,7 +8,8 @@ export const metadata: Metadata = {
 
 export default function home() {
   return (
-    <main className="w-full max-w-screen-xl mx-auto flex flex-wrap mt-10">
+    <main className="w-full max-w-screen-xl mx-auto flex flex-wrap mt-10 justify-center">
+      <About />
     </main>
   )
 }
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export * from './organism/Card';
 export * from './molecules/Header';
 export * from './molecules/Footer';
+export * from './molecules/About'
