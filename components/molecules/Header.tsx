1a50a30921d3a7ad7dc9263e8b485c5113c5b3fa
slesmes <santiagolesmesmarin@hotmail.com>
2024-09-15 22:29:53 -0500

    modificaciones para utilizar tokens

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
 import logo from "../../app/assets/images/pixelcut-export.png";
+import { colorBgblack } from '../tokens';
+import { colorTextWhite } from '../tokens';
 
 export function Header() {
     return (
-        <header className="bg-custom-gray p-4">
+        <header className = {`p-4 ${colorBgblack}`}>
             <nav className="flex justify-between items-center">
                 <div className="logo">
                     <a href="/home">
@@ -12,16 +14,17 @@ export function Header() {
                     </a>
                 </div>
                 <ul className="flex items-center space-x-6">
-                    <li><a href="/home" className='text-white hover:text-gray-300'>Home</a></li>
-                    <li><a href="/store" className='text-white hover:text-gray-300'>Store</a></li>
-                    <li><a href="/about" className='text-white hover:text-gray-300'>About</a></li>
+                    <li><a href="/home" className = {`${colorTextWhite}`} >Home</a></li>
+                    <li><a href="/store" className= {`${colorTextWhite}`} >Store</a></li>
+                    <li><a href="/about" className= {`${colorTextWhite}`} >About</a></li>
                     <li>
                         <a href="/register" className="bg-white text-gray-900 px-4 py-2 rounded-full hover:bg-gray-300">
                             Sign Up
                         </a>
                     </li>
                     <li>
-                        <a href="/login" className="bg-custom-gray text-white px-4 py-2 border border-white rounded-full hover:bg-gray-700">
+                        <a href="/login" className = {`${colorTextWhite} px-4 py-2 border border-white rounded-full hover:bg-gray-700 ${colorBgblack}`}>
+                        
                             Login
                         </a>
                     </li>
