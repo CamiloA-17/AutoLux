97262d83374778a9c85cdb38c4b65f156696ada3
juanesbrice11 <juanesbrice977@gmail.com>
2024-10-21 00:40:10 -0500

    Eliminando css layout y modificando estilos login y register

--- components/screeens/Login.tsx
+++ components/screeens/Login.tsx
@@ -9,6 +9,7 @@ import { auth } from "@/firebaseConfig";
 import { useRouter, usePathname } from "next/navigation";
 import { setCookie, getCookie } from 'typescript-cookie';
 import { useEffect, useState } from 'react';
+import backgroundImage from '../../app/assets/images/backgroundReg.png';
 
 type Inputs = {
   email: string;
@@ -19,7 +20,6 @@ export function Login() {
   const router = useRouter();
   const pathname = usePathname();
   const [token, setToken] = useState<string | null>(null);
-
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -53,59 +53,65 @@ export function Login() {
   if (token) {
     return <p>Redirigiendo...</p>;
   }
+  
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-[#212121] w-[500px] h-[450px] rounded-[30px]">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className={`mt-5 text-center text-2xl font-extrabold leading-9 tracking-tight ${colorTextWhite}`}>
-          Login
-        </h2>
-      </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label htmlFor="email" className={`block text-sm  font-medium leading-6 ${colorTextWhite}`}>
-              Correo electrónico
-            </label>
-            <div className="mt-2">
-              <input
-                {...register("email", { required: true })}
-                id="email"
-                type="email"
-                autoComplete="email"
-                placeholder="example@example.com"
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-              {errors.email && <p className="text-red-500 text-sm mt-2">{errors.email.message}</p>}
-            </div>
-          </div>
-          <div>
-            <label htmlFor="password" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
-              Contraseña
-            </label>
-            <div className="mt-2">
-              <input
-                {...register("password", { required: true })}
-                id="password"
-                type="password"
-                autoComplete="current-password"
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-              {errors.password && <p className="text-red-500 text-sm mt-2">{errors.password.message}</p>}
-            </div>
-          </div>
-          <button
-            type="submit"
-            className={`flex w-full justify-center rounded-md bg-[#424242] px-3 py-1.5 text-sm font-extrabold leading-6 ${colorTextWhite} shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
-          >
+    <div
+      className="flex min-h-screen flex-1 flex-col justify-center items-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage.src})` }}
+    >
+      <div className="flex flex-col justify-center items-center bg-[#212121] w-[500px] h-[450px] rounded-[30px] bg-opacity-80">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className={`mt-5 text-center text-2xl font-extrabold leading-9 tracking-tight ${colorTextWhite}`}>
             Login
-          </button>
-        </form>
-        <p className={`mt-10 text-center text-sm ${colorTextWhite}`}>
-          ¿No tienes cuenta?{' '}
-          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-            Regístrate
-          </a>
-        </p>
+          </h2>
+        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label htmlFor="email" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
+                Correo electrónico
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("email", { required: true })}
+                  id="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="example@example.com"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.email && <p className="text-red-500 text-sm mt-2">{errors.email.message}</p>}
+              </div>
+            </div>
+            <div>
+              <label htmlFor="password" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
+                Contraseña
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("password", { required: true })}
+                  id="password"
+                  type="password"
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.password && <p className="text-red-500 text-sm mt-2">{errors.password.message}</p>}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className={`flex w-full justify-center rounded-md bg-[#424242] px-3 py-1.5 text-sm font-extrabold leading-6 ${colorTextWhite} shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+            >
+              Login
+            </button>
+          </form>
+          <p className={`mt-10 text-center text-sm ${colorTextWhite}`}>
+            ¿No tienes cuenta?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Regístrate
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- components/screeens/Register.tsx
+++ components/screeens/Register.tsx
@@ -1,10 +1,11 @@
 'use client';
 import { colorTextWhite } from "../tokens";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { createUser } from "@/libs/api_users"; // Asegúrate de que esta función funcione correctamente
+import { createUser } from "@/libs/api_users";
 import { setCookie, getCookie } from 'typescript-cookie';
 import { useEffect, useState } from 'react';
 import { useRouter, usePathname } from "next/navigation";
+import backgroundImage from '../../app/assets/images/backgroundReg.png';
 
 type Inputs = {
   name: string;
@@ -38,170 +39,156 @@ export function Register() {
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     const { name, id, email, password } = data;
 
-    // Aquí estamos llamando a la función de creación de usuario
     const { success, message } = await createUser(name, id, email, password);
 
     if (success) {
       console.log('Usuario creado exitosamente:', message);
-      
-      // Redirigir después de crear el usuario
       router.push('/login');
     } else {
       console.error('Error al crear el usuario:', message);
+      alert(message);   
     }
   };
 
   if (token) {
-    return <p>Redirigiendo...</p>;
+    return <p>Redirigiendo...</p>; 
   }
 
   const password = watch("password");
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-[#212121] w-[500px] h-[670px] rounded-[30px]">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
-        <h2 className={`mt-5 text-center text-2xl font-extrabold leading-9 tracking-tight ${colorTextWhite}`}>
-          Register
-        </h2>
-      </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form
-          action="#"
-          method="POST"
-          className="space-y-6"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div>
-            <label
-              htmlFor="full-name"
-              className={`block text-sm font-medium leading-6 ${colorTextWhite}`}
-            >
-              Nombre Completo
-            </label>
-            <div className="mt-2">
-              <input
-                {...register("name", { required: true })}
-                id="full-name"
-                type="text"
-                autoComplete="name"
-                placeholder="John Doe"
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-            {errors.name && (
-              <span className="text-red-500 text-sm">Nombre requerido</span>
-            )}
-          </div>
-
-          <div>
-            <label
-              htmlFor="id"
-              className={`block text-sm font-medium leading-6 ${colorTextWhite}`}
-            >
-              Cédula
-            </label>
-            <div className="mt-2">
-              <input
-                {...register("id", { required: true })}
-                id="id"
-                type="text"
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-            {errors.id && (
-              <span className="text-red-500 text-sm">Cédula requerida</span>
-            )}
-          </div>
-
-          <div>
-            <label
-              htmlFor="email"
-              className={`block text-sm font-medium leading-6 ${colorTextWhite}`}
-            >
-              Correo electrónico
-            </label>
-            <div className="mt-2">
-              <input
-                {...register("email", { required: true })}
-                id="email"
-                type="email"
-                autoComplete="email"
-                placeholder="example@example.com"
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-            {errors.email && (
-              <span className="text-red-500 text-sm">Correo requerido</span>
-            )}
-          </div>
-
-          <div>
-            <label
-              htmlFor="password"
-              className={`block text-sm font-medium leading-6 ${colorTextWhite}`}
-            >
-              Contraseña
-            </label>
-            <div className="mt-2">
-              <input
-                {...register("password", { required: true })}
-                id="password"
-                type="password"
-                autoComplete="new-password"
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-            {errors.password && (
-              <span className="text-red-500 text-sm">Contraseña requerida</span>
-            )}
-          </div>
-
-          <div>
-            <label
-              htmlFor="confirm-password"
-              className={`block text-sm font-medium leading-6 ${colorTextWhite}`}
-            >
-              Confirmar contraseña
-            </label>
-            <div className="mt-2">
-              <input
-                {...register("confirmPassword", {
-                  required: true,
-                  validate: (value) =>
-                    value === password || "Las contraseñas no coinciden",
-                })}
-                id="confirm-password"
-                type="password"
-                autoComplete="new-password"
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-            {errors.confirmPassword && (
-              <span className="text-red-500 text-sm">
-                {errors.confirmPassword.message}
-              </span>
-            )}
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className={`flex w-full justify-center rounded-md bg-[#424242] px-3 py-1.5 text-sm font-extrabold leading-6 ${colorTextWhite} shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
-            >
-              Register
-            </button>
-          </div>
-        </form>
-
-        <p className={`mt-10 text-center text-sm ${colorTextWhite}`}>
-          ¿Ya tienes cuenta?{" "}
-          <a
-            href="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+    <div
+      className="flex min-h-screen flex-1 flex-col justify-center items-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage.src})` }}
+    >
+      <div className="flex flex-col justify-center items-center bg-[#212121] w-[500px] h-[670px] rounded-[30px] bg-opacity-80">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className={`mt-5 text-center text-2xl font-extrabold leading-9 tracking-tight ${colorTextWhite}`}>
+            Register
+          </h2>
+        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            className="space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
           >
-            Iniciar Sesión
-          </a>
-        </p>
+            <div>
+              <label htmlFor="full-name" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
+                Nombre Completo
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("name", { required: "Nombre requerido" })}
+                  id="full-name"
+                  type="text"
+                  autoComplete="name"
+                  placeholder="John Doe"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              {errors.name && (
+                <span className="text-red-500 text-sm">{errors.name.message}</span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="id" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
+                Cédula
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("id", { required: "Cédula requerida" })}
+                  id="id"
+                  type="text"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              {errors.id && (
+                <span className="text-red-500 text-sm">{errors.id.message}</span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="email" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
+                Correo electrónico
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("email", { required: "Correo requerido" })}
+                  id="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="example@example.com"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              {errors.email && (
+                <span className="text-red-500 text-sm">{errors.email.message}</span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="password" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
+                Contraseña
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("password", { required: "Contraseña requerida" })}
+                  id="password"
+                  type="password"
+                  autoComplete="new-password"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              {errors.password && (
+                <span className="text-red-500 text-sm">{errors.password.message}</span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="confirm-password" className={`block text-sm font-medium leading-6 ${colorTextWhite}`}>
+                Confirmar contraseña
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("confirmPassword", {
+                    required: "Confirmación requerida",
+                    validate: (value) =>
+                      value === password || "Las contraseñas no coinciden",
+                  })}
+                  id="confirm-password"
+                  type="password"
+                  autoComplete="new-password"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              {errors.confirmPassword && (
+                <span className="text-red-500 text-sm">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className={`flex w-full justify-center rounded-md bg-[#424242] px-3 py-1.5 text-sm font-extrabold leading-6 ${colorTextWhite} shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+              >
+                Registrar
+              </button>
+            </div>
+          </form>
+
+          <p className={`mt-10 text-center text-sm ${colorTextWhite}`}>
+            ¿Ya tienes cuenta?{" "}
+            <a
+              href="/login"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Iniciar Sesión
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
