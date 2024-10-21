517de59d7bba09912c556f1c32745d8a8899b9a5
juanesbrice11 <juanesbrice977@gmail.com>
2024-10-21 00:49:56 -0500

    Arreglando errores de redireccion en login y register

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,41 @@
+'use client'
 import { Metadata } from "next"
 import { Login } from "@/components/screeens/Login";
-
-export const metadata: Metadata = {
-  title: "Login",
-  description: "This is my login.",
-}
+import { useEffect, useState } from "react";
 
 export default function LoginForm() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false); 
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="loader"> 
+          <style jsx>{`
+            .loader {
+              border: 8px solid #f3f3f3; /* Color de fondo */
+              border-top: 8px solid #3498db; /* Color del spinner */
+              border-radius: 50%;
+              width: 60px;
+              height: 60px;
+              animation: spin 1s linear infinite;
+            }
+            @keyframes spin {
+              0% { transform: rotate(0deg); }
+              100% { transform: rotate(360deg); }
+            }
+          `}</style>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
+      
         <Login/>
     </>
   )
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,15 +1,44 @@
+'use client'
+
 import { Metadata } from "next"
 import { Register } from '@/components';
+import { useEffect, useState } from "react";
 
 
-export const metadata: Metadata = {
-  title: "Register",
-  description: "This is the place to register the users"
-}
 
 export default function register() {
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  setLoading(false); 
+}, []);
+
+if (loading) {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="loader"> 
+        <style jsx>{`
+          .loader {
+            border: 8px solid #f3f3f3; /* Color de fondo */
+            border-top: 8px solid #3498db; /* Color del spinner */
+            border-radius: 50%;
+            width: 60px;
+            height: 60px;
+            animation: spin 1s linear infinite;
+          }
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}</style>
+      </div>
+    </div>
+  );
+}
+
   return (
     <>
+    
       <Register/> 
     </>
   )
