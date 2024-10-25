931fb0bcf5e5f3e88aa933196bbdf86dfa98a7c2
juanesbrice11 <juanesbrice977@gmail.com>
2024-10-25 15:57:11 -0500

    Agregando ListBox de cambio de idioma en los componentes Header, Login y Register

---
+++ components/molecules/Language.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, Label } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+
+const languages = [
+  { code: 'en', name: 'English' },
+  { code: 'es', name: 'Español' },
+];
+
+export function LanguageSelector() {
+  const router = useRouter();
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
+
+  const handleLanguageChange = (language: any) => {
+    setSelectedLanguage(language);
+    document.cookie = `NEXT_LOCALE=${language.code}; path=/`;
+
+    // Obtener la ruta actual
+    const currentPath = window.location.pathname;
+    
+    // Reemplazar el código de idioma en la ruta actual
+    const newPath = currentPath.replace(/^\/(en|es)/, `/${language.code}`);
+    
+    // Redirigir a la nueva ruta
+    router.push(newPath);
+  };
+
+  // Efecto para recuperar el idioma actual al cargar el componente
+  useEffect(() => {
+    const locale = document.cookie
+      .split('; ')
+      .find(row => row.startsWith('NEXT_LOCALE='))
+      ?.split('=')[1];
+    
+    if (locale) {
+      const language = languages.find(lang => lang.code === locale) || languages[0];
+      setSelectedLanguage(language);
+    }
+  }, []);
+
+  return (
+    <div>
+      <Listbox value={selectedLanguage} onChange={handleLanguageChange}>
+        <div className="relative mt-2">
+          <ListboxButton className="relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+            <span className="ml-3 block truncate">{selectedLanguage.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+              <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
+            </span>
+          </ListboxButton>
+
+          <ListboxOptions className="absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {languages.map((language) => (
+              <ListboxOption
+                key={language.code}
+                value={language}
+                className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+              >
+                <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
+                  {language.name}
+                </span>
+
+                <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                  <CheckIcon aria-hidden="true" className="h-5 w-5" />
+                </span>
+              </ListboxOption>
+            ))}
+          </ListboxOptions>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
