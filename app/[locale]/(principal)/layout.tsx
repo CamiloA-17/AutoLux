e5c67d7ee61e0e1c6dab04a8521dfbfaf3956f53
slesmes <santiagolesmesmarin@hotmail.com>
2024-10-27 10:52:00 -0500

    haciendo que la ruta / sea igual a /home

--- app/[locale]/(principal)/layout.tsx
+++ app/[locale]/(principal)/layout.tsx
@@ -1,4 +1,10 @@
 import {Header, Footer} from '@/components';
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Home',
+    description: 'This is the home page',
+  };
 
 export default function AuthLayout({
     children
